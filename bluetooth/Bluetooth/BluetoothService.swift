ce3d4ab0c1f06f8cfb9a250a7bcf0595ef82fed3
Yutaro Konda <kondayutarou@gmail.com>
2023-02-23 20:23:38 +0900

    Set Notify

--- bluetooth/Bluetooth/BluetoothService.swift
+++ bluetooth/Bluetooth/BluetoothService.swift
@@ -9,15 +9,20 @@ import Foundation
 import CoreBluetooth
 import Combine
 
-final class BluetoothService: NSObject {
+final class BluetoothService: NSObject, CBPeripheralManagerDelegate {
     private (set)var centralManager: CBCentralManager?
+    private (set)var peripheralManager: CBPeripheralManager?
     @Published var cbManagerState: CBManagerState?
     @Published var didConnectPeripheral = false
     @Published var discoveredPeripherals: [CBPeripheral] = []
+    let LEDServiceUUID = "8c581e7e-b368-11ed-afa1-0242ac120002"
+    let LEDCharacteristicUUID = "a6fa1778-b368-11ed-afa1-0242ac120002"
+    var LEDCharacteristic: CBCharacteristic?
     
     override init() {
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
     }
     
     func startScan() {
@@ -41,5 +46,36 @@ extension BluetoothService: CBCentralManagerDelegate {
         if !discoveredPeripherals.contains(where: { $0.identifier == peripheral.identifier } ) {
             discoveredPeripherals.append(peripheral)
         }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+    }
+}
+
+extension BluetoothService: CBPeripheralDelegate {
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        peripheral.discoverCharacteristics(nil, for: (peripheral.services?.first)!)
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        service.characteristics?.forEach { characteristic in
+            if characteristic.uuid == CBUUID(string: LEDCharacteristicUUID) {
+                peripheral.setNotifyValue(true, for: (service.characteristics?[0])!)
+                LEDCharacteristic = characteristic
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        
     }
 }
